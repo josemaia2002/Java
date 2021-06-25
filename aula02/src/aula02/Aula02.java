a87fb806f8fb7ec411cbb23138b19affc5d74ebf
josemaia2002 <86317577+josemaia2002@users.noreply.github.com>
2021-06-25 15:47:12 -0300

    Update Aula02.java

--- aula02/src/aula02/Aula02.java
+++ aula02/src/aula02/Aula02.java
@@ -1,5 +1,15 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+package aula02;
+
+public class Aula02 {
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta();
+        c1.modelo = "Trilux";
+        c1.cor = "Azul";
+        c1.ponta = 0.5f;
+        c1.carga = 0;
+        c1.tampada = false;
+        c1.destampar();
+        c1.rabiscar();
+        c1.status();
     }
 }
