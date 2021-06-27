899d511cb39794d29c80250b49ea95633a86da30
josemaia2002 <86317577+josemaia2002@users.noreply.github.com>
2021-06-27 18:10:56 -0300

    Create Aula06.java

---
+++ aula06/src/aula06/Aula06.java
@@ -0,0 +1,9 @@
+package aula06;
+
+public class Aula06 {
+    public static void main(String[] args) {
+        ControleRemoto c1 = new ControleRemoto();
+        c1.ligar();
+        c1.abrirMenu();
+    }
+}
